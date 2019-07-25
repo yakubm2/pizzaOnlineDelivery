02b9e98b4d6b0bb889a64768a8f548ea4215c2c7
Durga Raghavapurapu <raod1989r@gmail.com>
2019-07-25 16:56:39 +0530

    initial commit

---
+++ src/main/java/com/example/tillproject/PizzaOnlineDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.tillproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOnlineDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOnlineDeliveryApplication.class, args);
+	}
+
+}
